d8460380134b2862926959f98d3bb268d764f533
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-12-23 21:22:09 +0100

    Implement path_link

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import WASI from "./wasi.js";
 export { WASI };
 
-export { Fd } from "./fd.js";
-export { Inode } from "./inode.js";
+export { Fd, Inode } from "./fd.js";
 export {
   File,
   SyncOPFSFile,
